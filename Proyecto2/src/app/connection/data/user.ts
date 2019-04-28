47849c37d35d38b04154d597daaa9068dcbf391e
MoraMaz <lestermaz.96@gmail.com>
2019-04-28 14:58:05 -0600

    Registrar e iniciar sesión

--- Proyecto2/src/app/connection/data/user.ts
+++
@@ -1,4 +0,0 @@
-export class User {
-    correo: string;
-    clave: string;
-}
--- Proyecto2/src/scripts/controllers/home.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-app.controller('HomeController', function(){
-    var vm = this;
-    console.log('Algo loco');
-});
